d2f1a0cd13107a22dd458873548559f75fc94a69
АСУС <natalja.rejenta@gmail.com>
2024-12-26 13:42:03 +0200

    Update OG02_ex.py
    change 2 to 3 (square to cube)

--- OG02_ex.py
+++ OG02_ex.py
@@ -2,13 +2,13 @@ spisok = [1, 2, 3, 4, 5 ]
 
 s = 0
 for i in range (len(spisok)):
-    s = s + spisok[i]**2
+    s = s + spisok[i]**3
 print(s)
 
 
 numbers = [1, 2, 3, 4, 5]
-sum_of_squares = sum(x**2 for x in numbers)
-print("Сумма квадратов элементов списка:", sum_of_squares)
+sum_of_cubes = sum(x**3 for x in numbers)
+print("Сумма кубов элементов списка:", sum_of_cubes)
 
 
 
